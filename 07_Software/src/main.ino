e7586b47765c451149718116e7ff330775182795
Franzelfx <fabian.franz0596@gmail.com>
2020-03-24 00:50:47 +0100

    prototype for software validation

--- 07_Software/src/main.ino
+++ 07_Software/src/main.ino
@@ -11,7 +11,7 @@
    fill in your spezific parameters here
  */
 #define DEBUG true
-// Ranges for the Parameters
+// ranges for the parameters
 #define RANGE_VOLUME 5
 #define RANGE_BREATHS 5
 #define RANGE_PROPORTION 5
@@ -19,21 +19,29 @@
 #define LCD_ADRESS 0x27
 #define LCD_COLUMNS 16
 #define LCD_ROWS 2
+// potentiometer pin defines
+#define POTI_1 A0
+#define POTI_2 A1
+#define POTI_3 A2
 // EEPROM defines
 #define ADRESS_VOLUME 0
 #define ADRESS_BREATHS 1
 #define ADRESS_PROPORTIONS 2
+// pressure sensor pin defines
+#define PRESSURE_UPPERSENSOR
+#define PRESSURE_LOWERSENSOR
+#define PRESSURE_ELECTRODE
 
-// konstruktors for the parts
+// construktors for the parts
 potentiometer poti1(A0, RANGE_VOLUME); // poti(pin, range);
 potentiometer poti2(A1, RANGE_BREATHS);
 potentiometer poti3(A2, RANGE_PROPORTION);
 LiquidCrystal_I2C display(LCD_ADRESS, LCD_COLUMNS, LCD_ROWS); // display(adress, columns, rows);
-pressureSensor sensor(9, 9, 9);								  // sensor(electodePin, topPin, bottomPin);
+pressureSensor sensor(3, 3, 3);								  // sensor(electodePin, topPin, bottomPin);
 
-// Variables for the Program sequence
+// variables for the Program sequence
 
-// Funtions for programm sequence
+// funtions for programm sequence
 void show(String topic, uint8_t value);
 void handlePotentiometers(uint8_t &volume, uint8_t &breath, uint8_t &proportions);
 void pressureTest();
@@ -44,13 +52,14 @@ void setup()
 	static uint8_t volume = EEPROM.read(ADRESS_VOLUME);
 	static uint8_t breath = EEPROM.read(ADRESS_BREATHS);
 	static uint8_t proportions = EEPROM.read(ADRESS_PROPORTIONS);
-	//initialise the display
+	// initialise the display
 	display.init();
 	display.backlight();
-	//initialise the serial port
+	// initialise the serial port
 	Serial.begin(9600);
-
-	//this
+	//#############################//
+	// main loop of the ventilator //
+	//#############################//
 	while (true)
 	{
 		handlePotentiometers(volume, breath, proportions);
@@ -61,7 +70,7 @@ void setup()
 // test functions
 void pressureTest()
 {
-	bool currState = sensor.stateOf(9);
+	bool currState = sensor.stateOf(3);
 	static bool prevState;
 
 	if (currState != prevState)
