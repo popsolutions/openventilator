8f5b7a7fbc790b31e4fa6c676ea59fe59dd994ec
Franzelfx <fabian.franz0596@gmail.com>
2020-03-25 23:46:38 +0100

    motor encoder functions

--- 07_Software/src/main.ino
+++ 07_Software/src/main.ino
@@ -2,10 +2,10 @@
  * This is the main Programm of the Ventilator.
  */
 #include <Arduino.h>
+#include <EEPROM.h>
+#include <LiquidCrystal_I2C.h> // By Frank The Brabaner
 #include "potentiometer.h"
 #include "pressureSensor.h"
-#include <LiquidCrystal_I2C.h> // By Frank The Brabaner
-#include "EEPROM.h"
 #include "dcMotor.h"
 
 /* 
@@ -13,9 +13,9 @@
  */
 #define DEBUG true
 // ranges for the parameters
-#define RANGE_VOLUME 5
-#define RANGE_BREATHS 5
-#define RANGE_PROPORTION 5
+#define RANGE_VOLUME 10
+#define RANGE_BREATHS 10
+#define RANGE_PROPORTION 10
 // LCD defines
 #define LCD_ADRESS 0x27
 #define LCD_COLUMNS 16
@@ -34,24 +34,24 @@
 #define PRESSURE_BOTTOMENSOR 6
 // motor pin defines
 #define MOTOR_POWER 4
-#define MOTOR_TOPSENSOR 2
-#define MOTOR_BOTTOMSENSOR 3
-#define MOTOR_TOPSWITCH 11
-#define MOTOR_BOTTOMSWITCH 12
+#define MOTOR_SENSOR 2
 
-// construktors for the parts
+// constructors for the parts
 potentiometer poti1(A0, RANGE_VOLUME); // poti(pin, range);
 potentiometer poti2(A1, RANGE_BREATHS);
 potentiometer poti3(A2, RANGE_PROPORTION);
-LiquidCrystal_I2C display(LCD_ADRESS, LCD_COLUMNS, LCD_ROWS); // display(adress, columns, rows);
-dcMotor motor(MOTOR_POWER, MOTOR_TOPSENSOR, MOTOR_BOTTOMSENSOR, MOTOR_TOPSWITCH, MOTOR_BOTTOMSWITCH);
+LiquidCrystal_I2C display(LCD_ADRESS, LCD_COLUMNS, LCD_ROWS);						 // display(adress, columns, rows);
+dcMotor motor(MOTOR_POWER, MOTOR_SENSOR);											 // motor(motorPin, sensorPin);
 pressureSensor sensor(PRESSURE_ELECTRODE, PRESSURE_TOPSENSOR, PRESSURE_BOTTOMENSOR); // sensor(electodePin, topPin, bottomPin);
 
 // variables for the Program sequence
 
 // function for code validating
-void testEndSwitch();
-void testEndSensor();
+void testMotor()
+{
+	if (motor.rotate())
+		Serial.println(motor.getRpm());
+}
 // funtions for programm sequence
 void show(String topic, uint8_t value);
 void handlePotentiometers(uint8_t &volume, uint8_t &breath, uint8_t &proportions);
@@ -73,32 +73,11 @@ void setup()
 	while (true)
 	{
 		handlePotentiometers(volume, breath, proportions);
-		testEndSwitch();
-		testEndSensor();
+		testMotor();
 	}
 }
 
 // test functions
-void testEndSwitch()
-{
-	static int count = 0;
-	if (motor.endSwitch())
-	{
-		Serial.print("endSwitch: ");
-		count++;
-		Serial.println(count);
-	}
-}
-void testEndSensor()
-{
-	static int count = 0;
-	if (motor.endSensor())
-	{
-		Serial.print("endSensor: ");
-		count++;
-		Serial.println(count);
-	}
-}
 // head functions
 void handlePotentiometers(uint8_t &volume, uint8_t &breath, uint8_t &proportions)
 {
@@ -124,6 +103,9 @@ void show(String topic, uint8_t value)
 	display.clear();
 	display.setCursor(0, 0); // setCourser(column, row);
 	display.print(topic);
-	display.setCursor(15, 0);
+	if (value < 10)
+		display.setCursor(15, 0);
+	else
+		display.setCursor(14, 0);
 	display.print(value);
 }
